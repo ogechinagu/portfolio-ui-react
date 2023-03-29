e34f32f39b80fbcdbd8a43a3e4eb3b1ac65979dd
Ogechi Ikwunagu <mzogechee@gmail.com>
2023-03-29 21:59:42 +0100

    Creating Sidnav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import { AppRoutes } from './utils/Routes'
+import './App.scss'
+
 function App() {
   return (
     <>
-      <h1>Portfolio</h1>
+      <Routes>
+        <Route path={AppRoutes.home} element={<Layout />} />
+      </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Navigation/Sidebar.jsx
+++ src/components/Navigation/Sidebar.jsx
@@ -1,11 +1,84 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { AppRoutes } from '../../utils/Routes'
+import {
+  FaHome,
+  FaUser,
+  FaRegLightbulb,
+  FaEnvelope,
+  FaSuitcase,
+  FaBars,
+  FaClose,
+} from 'react-icons/fa'
+import { BsGear } from 'react-icons/bs'
+import myLogo from '../../assets/images/my-logo-oi.png'
+import './Sidebar.scss'
 
-const Sidebar = () =>  (
-    <div className='nav-bar'><Link className='logo' to={AppRoutes.home}>
-    </Link></div>
+const Sidebar = () => {
+  const [showNav, setShowNav] = React.useState(false)
+
+  const navItems = [
+    {
+      name: 'Home',
+      className: '',
+      icon: <FaHome />,
+      path: AppRoutes.home,
+    },
+    {
+      name: 'About',
+      className: 'about-link',
+      icon: <FaUser />,
+      path: AppRoutes.about,
+    },
+    {
+      name: 'Skills',
+      className: 'skills-link',
+      icon: <BsGear />,
+      path: AppRoutes.skills,
+    },
+    {
+      name: 'Tips',
+      className: 'tips-link',
+      icon: <FaRegLightbulb />,
+      path: AppRoutes.tips,
+    },
+    {
+      name: 'My Work',
+      className: 'mywork-link',
+      icon: <FaSuitcase />,
+      path: AppRoutes.mywork,
+    },
+    {
+      name: 'Contact',
+      className: 'contact-link',
+      icon: <FaEnvelope />,
+      path: AppRoutes.contact,
+    },
+  ]
+
+  const smItems = [{}]
+
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to={AppRoutes.home}>
+        <img src={myLogo} alt="logo" />
+        <h6 className="logo-name">Ogechi Ikwunagu</h6>
+      </Link>
+      <nav>
+        {navItems.map((item, index) => (
+          <NavLink
+            // exact="true"
+            activeclassname="active"
+            className={item.className}
+            to={item.path}
+            onClick={() => setShowNav(false)}
+          >
+            <span style={{ color: '#4d4d4e !important' }}>{item.icon}</span>
+          </NavLink>
+        ))}
+      </nav>
+    </div>
   )
-
+}
 
 export default Sidebar
